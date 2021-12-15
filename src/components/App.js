6825f1b28515fd0a638aba16b6b5295b88e25bed
abdowahbah <abdowahbah123@yahoo.com>
2021-12-15 14:16:54 +0200

    initial search

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,22 @@ import Search from "./search/Search";
 import axios from "axios";
 import RecipeList from "./Food/RecipeList";
 import RecipeDetail from "./Food/RecipeDetail";
+import { useEffect } from "react/cjs/react.development";
 
 const App = () =>
 {
     const [recipes, setRecipes] = useState([]);
     const [selectedRecipe, setSelectedRecipe] = useState(null);
+
+    useEffect(()=>
+    {
+        const search = async () =>
+        {
+            const response = await axios.get(`https://forkify-api.herokuapp.com/api/search?q=pizza`); // default (initial) search
+            setRecipes(response.data.recipes)
+        }
+        search();
+    },[])
 
     const onFormSubmit = async (term) =>
     {
